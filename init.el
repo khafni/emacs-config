49bc00143da38970b5cda4bd4d3b3634e0661429
khafni <khafni@student.1337.ma>
2020-02-09 05:12:07 +0100

    ok first commit

---
+++ init.el
@@ -0,0 +1,49 @@
+; start package.el with emacs
+(require 'package)
+; add MELPA to repository list
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+; initialize package.el
+(package-initialize)
+; start auto-complete with emacs
+(require 'auto-complete)
+; do default config for auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+; start yasnippet with emacs
+(require 'yasnippet)
+(yas-global-mode 1)
+; let's define a function which initializes auto-complete-c-headers and gets called for c/c++ hooks
+(defun my:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include")
+)
+; now let's call this function from c/c++ hooks
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fancy-splash-image nil)
+ '(package-selected-packages
+   (quote
+    (random-splash-image yasnippet auto-complete-c-headers))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;(load "~/.emacs.d/random-splash-image.el")
+;(require 'random-splash-image)
+;(setq random-splash-image-dir (concat (getenv "HOME") "/.emacs.d/splash-images"))
+;(random-splash-image-set)
+;(setq fancy-splash-image (create-image "~/gg.gif")
+(setq inhibit-startup-screen t)
+;(insert-image (create-image "~/gg.gif"))
+
+;; my elisp :) :) :)
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
